63f00931a5377f2d160e7ee585dcb9f8ced07324
Zhixiang Fu <zhixiangfu1106@gmail.com>
2021-11-21 18:55:00 -0500

    users collections

---
+++ data/index.js
@@ -0,0 +1,9 @@
+const managers = require("./managers");
+const users = require("./users");
+
+// just list some of the data.js here
+// const restauarants = require("./restauarants");
+// const food_items = require("./food_items");
+// const reviews = require("./reviews");
+// const replies = require("./reviews");
+// const orders_placed = require("./reviews");
