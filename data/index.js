48c664e7220730e1451c41747e327f6415cfe7e6
Zhixiang Fu <zhixiangfu1106@gmail.com>
2021-12-02 18:05:56 -0500

    update users and mangers.js

--- data/index.js
+++ data/index.js
@@ -1,9 +1,5 @@
 const managers = require("./managers");
 const users = require("./users");
-
-// just list some of the data.js here
-// const restauarants = require("./restauarants");
-// const food_items = require("./food_items");
-// const reviews = require("./reviews");
-// const replies = require("./reviews");
-// const orders_placed = require("./reviews");
+const restauarants = require("./restaurants");
+const reviews = require("./reviews");
+const replies = require("./replies");
